1d2f53a0d4f5c7dc76bf6562791eda00f898b12c
Tejas <60872437+tejasraibagi26@users.noreply.github.com>
2022-02-13 23:20:49 -0500

    Web chat using socket

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import io from "socket.io-client";
+import "./App.css";
+import Chat from "./components/chat/chat";
+import Login from "./components/login/login";
 
-function App() {
+let socket;
+
+const App = () => {
+  const ENDPOINT = "http://localhost:8080";
+
+  const [username, setUsername] = useState("");
+
+  //   useEffect(() => {
+  //     if (!username) {
+  //       let user = prompt("Enter Username", "");
+  //       setUsername(user);
+  //     }
+  //     socket = io(ENDPOINT);
+
+  //     socket.emit("join", { username, roomName: "Test" });
+
+  //     return () => socket.close();
+  //   }, [ENDPOINT, username]);
+
+  //   useEffect(() => {
+  //     socket.on("chat-message", (msg) => {
+  //       setMessages([...messages, msg]);
+  //     });
+  //   });
+
+  //   const onChatMessageChange = (e) => {
+  //     setText(e.target.value);
+  //   };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/chat" element={<Chat />} />
+      </Routes>
+    </Router>
+    // <section>
+    //   {messages.map((m) => {
+    //     return (
+    //       <div className="msg" key={Math.random()}>
+    //         <strong>{m.username}</strong>: {m.text}
+    //       </div>
+    //     );
+    //   })}
+    //   <input
+    //     type="text"
+    //     id="textMsg"
+    //     placeholder="Enter text"
+    //     onChange={onChatMessageChange}
+    //   />
+    //   <button type="submit" onClick={onSubmit}>
+    //     Send
+    //   </button>
+    // </section>
   );
-}
+};
 
 export default App;
